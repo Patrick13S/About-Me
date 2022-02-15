f63f46f7457713aa92a3d90abb0fd7c2bc5a9bc8
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-02-15 15:34:58 -0700

    name

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -1,19 +1,47 @@
 //
 //  ViewController.swift
-//  About Me
+//  About Patrick Stroyan
 //
 //  Created by Patrick Stroyan on 2/15/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+  
 
-    override func viewDidLoad() {
+  
+    @IBOutlet weak var aboutMe: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        aboutMe.text = "Patrick"
+        
     }
-
-
+    
+    var x = 0
+    @IBAction func button(_sender: UIButton)
+        {
+          //  x+=1
+        //    if x == 1 { myLmageView.image = UIImage (named: "kirby")
+                
+            
+                
+               
+                
+            }
+       //     else if x==2 {
+           // myLmageView.image = UIImage (named: "Cute cat")
+         //   } else {
+             //   x=0
+              //  myLmageView.image = UIImage (named: "kirby")
+    
+            
 }
+    
+
 
