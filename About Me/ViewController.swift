35bbd51d90d369a11b8cdbb33e04dd14e39364cc
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-02-15 14:29:49 -0700

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Patrick Stroyan on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
