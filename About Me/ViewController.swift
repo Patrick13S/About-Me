9a7cb4a4106426c122c34193275a1e33a8e61dc9
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-02-15 15:43:19 -0700

    button

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -26,22 +26,23 @@ class ViewController: UIViewController
     var x = 0
     @IBAction func button(_sender: UIButton)
         {
-          //  x+=1
+           x+=1
         //    if x == 1 { myLmageView.image = UIImage (named: "kirby")
-                
-            
+            if x == 1 {
+                aboutMe.text = "I like cats."
                 
                
                 
             }
-       //     else if x==2 {
+       else if x==2 {  aboutMe.text = "Patrick"
            // myLmageView.image = UIImage (named: "Cute cat")
-         //   } else {
-             //   x=0
+          } else {
+              x=0
+              aboutMe.text = "Patrick"
               //  myLmageView.image = UIImage (named: "kirby")
-    
+          }
             
 }
     
 
-
+        }
