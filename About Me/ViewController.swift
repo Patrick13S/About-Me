632b1d40763966756e4c0a918a66c2d3c4d5f5f9
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-03-08 15:38:06 -0700

    more text

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -20,29 +20,57 @@ class ViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         aboutMe.text = "Patrick"
-        
+        myLmageView.image = UIImage (named: "patrick1")
     }
+    @IBOutlet weak var myLmageView: UIImageView!
     
     var x = 0
     @IBAction func button(_sender: UIButton)
         {
            x+=1
-        //    if x == 1 { myLmageView.image = UIImage (named: "kirby")
+        
+            myLmageView.image = UIImage (named: "patrick3")
             if x == 1 {
                 aboutMe.text = "I like cats."
-                
+               
                
                 
             }
-       else if x==2 {  aboutMe.text = "Patrick"
-           // myLmageView.image = UIImage (named: "Cute cat")
-          } else {
+    
+    else{
               x=0
               aboutMe.text = "Patrick"
-              //  myLmageView.image = UIImage (named: "kirby")
+              myLmageView.image = UIImage (named: "patrick1")
+            
           }
             
 }
+  
+
+  @IBOutlet weak var segmentedcontroller: UISegmentedControl!
     
 
+    
+    @IBAction func backgroundcolor(_ sender: Any) {
+    
+switch segmentedcontroller.selectedSegmentIndex
+    
+{
+   case 0: self.view.backgroundColor = UIColor.white
+   
+    
+   case 1: self.view.backgroundColor = UIColor.blue
+        
+        
+case 2: self.view.backgroundColor = UIColor.systemTeal
+        
+case 3: self.view.backgroundColor = UIColor.green
+   default: self.view.backgroundColor = UIColor.white
+       
         }
+  }
+    
+    
+    
+        
+}
